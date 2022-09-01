302772fc795c85a7b5efa750f68eb6cb47cc3eed
nyoung03 <vnfdlvdk37@gmail.com>
2022-09-01 19:07:26 +0900

    Stack 파트 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Profile from "./components/Profile";
 import ProjectList from "./components/ProjectList";
+import Stack from "./components/Stack";
 
 function App() {
   return (
     <>
       <Header />
       <Profile />
+      <Stack />
       <ProjectList />
-      <Contact />
     </>
   );
 }
