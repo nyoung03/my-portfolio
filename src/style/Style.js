acc0f3f1e6a9db40a58cd124423f3b9513353b76
nyoung03 <vnfdlvdk37@gmail.com>
2022-09-05 19:16:11 +0900

    글꼴 추가

--- src/style/Style.js
+++ src/style/Style.js
@@ -71,6 +71,12 @@ function Style() {
             href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap"
             rel="stylesheet"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap"
+            rel="stylesheet"
+          ></link>
         </Helmet>
       </HelmetProvider>
     </>
